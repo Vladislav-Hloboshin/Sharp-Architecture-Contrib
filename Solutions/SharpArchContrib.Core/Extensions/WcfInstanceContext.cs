4e6078a9a0580c2d703149da75b842d774c32ee1
Vladislav-Hloboshin <474b894585>
2015-03-24 21:20:21 +0300

    changed store location for TransactionDepth from ThreadStatic to OperationContext changed log4net version

---
+++ Solutions/SharpArchContrib.Core/Extensions/WcfInstanceContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace SharpArchContrib.Core.Extensions
+{
+    public class WcfInstanceContext : IExtension<InstanceContext>
+    {
+        private WcfInstanceContext()
+        {
+        }
+
+        ///<summary>
+        /// Stored in current instance context.
+        ///</summary>
+        public int TransactionDepth { get; set; }
+
+        ///<summary>
+        /// Gets the current instance of <see cref="WcfInstanceContext"/>
+        ///</summary>
+        public static WcfInstanceContext Current
+        {
+            get
+            {
+                if (OperationContext.Current == null || OperationContext.Current.InstanceContext == null)
+                {
+                    return null;
+                }
+                WcfInstanceContext context = OperationContext.Current.InstanceContext.Extensions.Find<WcfInstanceContext>();
+                if (context == null)
+                {
+                    context = new WcfInstanceContext();
+                    OperationContext.Current.InstanceContext.Extensions.Add(context);
+                }
+                return context;
+            }
+        }
+
+        /// <summary>
+        /// <see cref="IExtension{T}"/> Attach() method
+        /// </summary>
+        public void Attach(InstanceContext owner) { }
+
+        /// <summary>
+        /// <see cref="IExtension{T}"/> Detach() method
+        /// </summary>
+        public void Detach(InstanceContext owner) { }
+    }
+}
--- Solutions/SharpArchContrib.Data/NHibernate/TransactionManagerBase.cs
+++ Solutions/SharpArchContrib.Data/NHibernate/TransactionManagerBase.cs
@@ -1,4 +1,6 @@
-﻿namespace SharpArchContrib.Data.NHibernate
+﻿using SharpArchContrib.Core.Extensions;
+
+namespace SharpArchContrib.Data.NHibernate
 {
     using System;
     using System.Threading;
@@ -8,10 +10,10 @@
     [Serializable]
     public abstract class TransactionManagerBase : ITransactionManager
     {
-        private static readonly ILog logger = LogManager.GetLogger(typeof(TransactionManagerBase));
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(TransactionManagerBase));
 
         [ThreadStatic]
-        private static int transactionDepth;
+        private static int _transactionDepth;
 
         public abstract string Name { get; }
 
@@ -19,7 +21,12 @@
         {
             get
             {
-                return transactionDepth;
+                var currentWcfContext = WcfInstanceContext.Current;
+                if (currentWcfContext == null)
+                {
+                    return _transactionDepth;
+                }
+                return currentWcfContext.TransactionDepth;
             }
         }
 
@@ -27,15 +34,33 @@
 
         public virtual object PopTransaction(string factoryKey, object transactionState)
         {
-            Interlocked.Decrement(ref transactionDepth);
-            this.Log(string.Format("Pop Transaction to Depth {0}", transactionDepth));
+            var currentWcfContext = WcfInstanceContext.Current;
+            if (currentWcfContext == null)
+            {
+                Interlocked.Decrement(ref _transactionDepth);
+                Log(string.Format("[ThreadStatic] Pop Transaction to Depth {0}", _transactionDepth));
+            }
+            else
+            {
+                currentWcfContext.TransactionDepth--;
+                Log(string.Format("[OperationContext({0})] Pop Transaction to Depth {1}", currentWcfContext.GetHashCode(), currentWcfContext.TransactionDepth));
+            }
             return transactionState;
         }
 
         public virtual object PushTransaction(string factoryKey, object transactionState)
         {
-            Interlocked.Increment(ref transactionDepth);
-            this.Log(string.Format("Push Transaction to Depth {0}", transactionDepth));
+            var currentWcfContext = WcfInstanceContext.Current;
+            if (currentWcfContext == null)
+            {
+                Interlocked.Increment(ref _transactionDepth);
+                Log(string.Format("[ThreadStatic] Push Transaction to Depth {0}", _transactionDepth));
+            }
+            else
+            {
+                currentWcfContext.TransactionDepth++;
+                Log(string.Format("[OperationContext({0})] Push Transaction to Depth {1}", currentWcfContext.GetHashCode(), currentWcfContext.TransactionDepth));
+            }
             return transactionState;
         }
 
@@ -45,7 +70,7 @@
 
         protected void Log(string message)
         {
-            logger.Debug(string.Format("{0}: {1}", this.Name, message));
+            Logger.Debug(string.Format("{0}: {1}", this.Name, message));
         }
     }
 }
